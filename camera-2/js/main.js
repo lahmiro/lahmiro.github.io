a6e2d6dbe9f11ae3478243f27df48fe5eaec5d8f
lahmiro <380213218@qq.com>
2015-02-15 19:45:18 -0500

    update aim

--- camera-2/js/main.js
+++ camera-2/js/main.js
@@ -1,13 +1,31 @@
 var svg = new Walkway({
   selector: '#camera',
-  duration: '3000',
+  duration: '2500',
   // can pass in a function or a string like 'easeOutQuint'
   easing: function (t) {
     return  t<.5 ? 2*t*t : -1+(4-2*t)*t;
   }
 });
 
+var svg2 = new Walkway({
+  selector: '#timeline',
+  duration: '1000',
+  // can pass in a function or a string like 'easeOutQuint'
+  easing: function (t) {
+    return  t<.5 ? 2*t*t : -1+(4-2*t)*t;
+  }
+});
+
+
 svg.draw();
+svg2.draw();
+
+function popup(url) {
+	newwindow=window.open(url,'name','height=500,width=600');
+	if (window.focus) {newwindow.focus()}
+	return false;
+}
+
 
 $(document).ready(function($) {
 
@@ -61,7 +79,39 @@ $(document).ready(function($) {
 
 
 	//flipping cube
-	
+	/* 
+		flip everytime an image enters
+		change text (store in an array)
+		an array of scroll scene
+	*/
+	//hover to change text
+
+	/*
+	$('.default').hover(
+	    function() {
+	        var $this = $(this); 
+	        $this.data('initialText', $this.text());
+	        $this.text("I'm replaced!").addClass("textTransition");
+	    },
+	    function() {
+	        var $this = $(this); 
+	        $this.text($this.data('initialText'));
+	    }
+	);
+
+*/
+
+	function changeCaption(){
+		var originText = $(".default");
+		originText.data('initalText', originText.text());
+		originText.text("Replaced").addClass("textTransition");
+	}
+
+	var flip01 = new ScrollScene({triggerElement:"#imgContainer", offset: 180, duration: 30})
+			.setTween(TweenMax.to("#cube", 0.4, {rotationX:90, ease: Power1.easeOut,  onComplete: changeCaption}));
+
+	var flip02 = new ScrollScene({triggerElement:"#imgContainer", offset: 302, duration: 30})
+			.setTween(TweenMax.to("#cube", 0.4, {rotationX:-10, transformOrigin: "bottom center", ease: Power1.easeOut}));
 
 	controller.addScene([
 		pinCamera,
@@ -70,6 +120,8 @@ $(document).ready(function($) {
 		movingDot,
 		passingDot2013,
 		passingDot2014,
+		flip01,
+		flip02,
 	]);
 
 	// init controller for imgSlider
@@ -103,4 +155,77 @@ $(document).ready(function($) {
 
 	//circleTween.addIndicators();
 	//bgLeftToRight.addIndicators();
+
+	//arrows anim at the start
+	var btn = $(".clickItems");
+	var btnTl = [];
+	for(var i=0, l=btn.length; i<l; i++){
+		btnTl[i] = new TimelineMax();
+		btnTl[i].to(btn[i],0.1,{y:2, ease: Power0.easeNone, delay:2.5});
+		btnTl[i].to(btn[i], 0.1,{y:0, strokeWidth: "1.5", ease: Power0.easeNone});
+		btnTl[i].to(btn[i],0.1,{strokeWidth:"1", ease: Power0.easeNone});
+	}
+
+	//other btns
+	
+
+	//click arrows anim
+	$(".clickItems").click(clickHandler);
+	function clickHandler(){
+		var newTl = new TimelineMax();
+			newTl.to($(this), 0.1, {y:2, strokeWidth: "1.5", ease: Power0.easeNone});
+			newTl.to($(this), 0.05, {y:0, strokeWidth:"1", ease: Power0.easeNone});
+	};
+
+	//arrow key anim when hover
+	var rotateStill = new TweenMax.to("#arrowsBtn", 1, {strokeWidth: "1", rotation: "+=0", ease: Bounce.easeOut, transformOrigin:"50% 43%"});
+	$(".clickItems").hover(
+		function(){
+			if(($(this).is("#downArrow")) || ($(this).is("#centerArrow"))){
+				TweenMax.to("#arrowsBtn", 1, {strokeWidth: "1.3", rotation: "0", ease: Bounce.easeOut, transformOrigin:"50% 43%"});
+			}
+			if($(this).is("#upArrow")){
+				TweenMax.to("#arrowsBtn", 1, {strokeWidth: "1.3", rotation: "-180", ease: Bounce.easeOut, transformOrigin:"50% 43%"});
+			}
+			if($(this).is("#leftArrow")){
+				TweenMax.to("#arrowsBtn", 0.5, {strokeWidth: "1.3", rotation: "90", ease: Bounce.easeOut, transformOrigin:"50% 43%"});
+			}
+			if($(this).is("#rightArrow")){
+				TweenMax.to("#arrowsBtn", 0.5, {strokeWidth: "1.3", rotation: "-90", ease: Bounce.easeOut, transformOrigin:"50% 43%"});
+			}
+		},function(){
+				rotateStill;
+		}
+	);
+
+	
+	//rotate FB and Twitter Btn when hover
+	$(".rotateBtn").hover(
+		function(){
+			TweenMax.to($(this), 1, {fill: "white", rotation: "360", ease: Bounce.easeOut, transformOrigin: "50% 50%"});
+			
+		},function(){
+			TweenMax.to($(this), 1, {fill: "#3498db",rotation: "0", ease: Bounce.easeOut, transformOrigin: "50% 50%"});
+		}
+	);
+
+	
+	//rotating gear anim
+	$("#AEAFBtn").hover(
+		function(){
+			TweenMax.to($(this), 6, {rotation: "+=100", ease:Linear.easeNone, transformOrigin: "50% 50%"});
+		},function(){
+			TweenMax.to($(this), 2.5, {rotation: "+=20", ease: Power1.easeOut, transformOrigin: "50% 50%"});
+		}
+	);
+
+	//rotate LvBtn
+	$("#LvBtn").mouseover(
+		function(){
+			TweenMax.to("#LvBtn", 0.5, {rotation: "+=60", ease: Bounce.easeOut, transformOrigin:"40% 48%"});
+		}
+	); 
+
+
+
 });	
